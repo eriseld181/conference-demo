7ea2c7f0aafb81f8c6139a62bee576ff80f48ef5
erise <eriseld181@gmail.com>
2021-04-19 14:48:22 +0200

    first commit

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,21 @@
+package com.pluralsight.conferencedemo.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration 
+public class PersistenceConfiguration {
+	@Bean
+	public DataSource dataSource() {
+		DataSourceBuilder builder = DataSourceBuilder.create();
+		builder.url("jdbc:mysql://localhost:3306/conference_demo");
+		builder.username("root");
+		builder.password("Troy1894!");
+		System.out.println("eriseld method");
+		return builder.build();
+	}
+
+}
