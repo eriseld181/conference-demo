0ea92ec1bda4f957d4a62218806171ec67d5c125
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2021-04-19 15:43:13 +0200

    Update PersistenceConfiguration.java

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,14 +8,14 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration 
 public class PersistenceConfiguration {
-	@Bean
-	public DataSource dataSource() {
-		DataSourceBuilder builder = DataSourceBuilder.create();
-		builder.url("jdbc:mysql://localhost:3306/conference_demo");
-		builder.username("root");
-		builder.password("Troy1894!");
-		System.out.println("eriseld method");
-		return builder.build();
-	}
+// 	@Bean
+// 	public DataSource dataSource() {
+// 		DataSourceBuilder builder = DataSourceBuilder.create();
+// 		builder.url("jdbc:mysql://localhost:3306/conference_demo");
+// 		builder.username("root");
+// 		builder.password("Troy1894!");
+// 		System.out.println("eriseld method");
+// 		return builder.build();
+// 	}
 
 }
